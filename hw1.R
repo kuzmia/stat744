2392f79200d5bc38196c6a1211d99a11cc5e742d
Ben Bolker <bbolker@gmail.com>
2018-01-20 15:33:32 -0500

    BMB comments on HW1

--- hw1.R
+++ hw1.R
@@ -8,8 +8,19 @@ ggplot(data=mutate(sub_ais,value=1))+ #adding a value variable to count athletes
   coord_flip()+ #flips coordinates to form horizontal bar graph
   facet_wrap(~sex) #compare by gender
 
-"Alligning the bars horizontally allows for easier comparison between the number of individuals per sport.
-Immediately, you can tell that there was no woman's waterpolo team, and no men's Netball and Gymnastics team."
+## BMB: IMO (and Cleveland's), better to align along common axis. Colors are redundant with axis
+##  position anyway
+## https://stackoverflow.com/questions/10326729/dont-drop-zero-count-dodged-barplot
+ggplot(data=mutate(sub_ais,value=1))+ #adding a value variable to count athletes in their respective sports
+  geom_bar(mapping = aes(x=sport, fill = sex),
+           position=position_dodge(preserve="single")) +
+  coord_flip() #flips coordinates to form horizontal bar graph
+
+## BMB: please use actual R comments (#)
+
+## "Alligning the bars horizontally allows for easier comparison between the number of individuals per sport.
+## Immediately, you can tell that there was no woman's waterpolo team, and no men's Netball and Gymnastics team."
+
 
 
 #2.Creates a box plot comparing hemoglobin concentrations by sport
@@ -17,10 +28,19 @@ ggplot(data=ais, aes(x = sport,y = hg))+
   geom_boxplot()+
   facet_wrap(~sex)
 
-"The authors analyzing the dataset were interested in seeing if endurance-intensive sports consisted of athletes
-that had higher hemoglobin concentrations than those in power-intensive sports. For instance, the median hemoglobin concentration is higher in female track and field
-athletes when compared to the gymnastics team. Using facet_wrap(), it appears that men tend to
-have higher hemoglobin concentrations than women for their respective sports." 
+
+## BMB: why did you leave 'sport' alphabetically ordered? why not reorder
+## by mean/median hg?
+## coord_flip() would make it easier to read sport tick-mark labels
+
+## "The authors analyzing the dataset were interested in seeing if endurance-intensive sports consisted of athletes
+## that had higher hemoglobin concentrations than those in power-intensive sports. For instance, the median hemoglobin concentration is higher in female track and field
+## athletes when compared to the gymnastics team. Using facet_wrap(), it appears that men tend to
+## have higher hemoglobin concentrations than women for their respective sports.
+
+## BMB: if your primary goal is the M/F comparison you shouldn't split by
+## facets: instead, colour the boxplots (or fill them) and let ggplot
+## dodge them for you
 
 #3.Creates a scatterplot that illustrates correlation between red blood cell count and hemoglobin concentration
 ggplot(data=ais)+
@@ -28,5 +48,11 @@ ggplot(data=ais)+
   geom_smooth(method = "lm", aes(x=rcc, y=hg))+
   facet_wrap(~sex)
 
-"A strong positive correlation is not surprising since hemoglobin is a protein found in red blood cells.
-I decided to add colour to determine any sports that potential outliers may come from."
+##"A strong positive correlation is not surprising since hemoglobin is a protein found in red blood cells.
+## I decided to add colour to determine any sports that potential outliers may come from."
+
+## BMB: OK.
+##  Why did you choose method="lm" rather than standard loess?
+##  If you specify x=rcc, y=hg mappings in the original ggplot call,
+##   you don't need to specify them for each geom -- they will be
+##   inherited
