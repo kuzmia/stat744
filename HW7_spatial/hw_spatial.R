fecfd309c7efe89a7aae94b5150365da97ce48a1
kuzmia <kuzmia@users.noreply.github.com>
2018-04-01 19:57:56 -0400

    html was too big -- pgn instead

--- HW7_spatial/hw_spatial.R
+++ HW7_spatial/hw_spatial.R
@@ -1,17 +1,17 @@
 library(leaflet)
 
 df <- read.csv("database.csv") #dataset of Earthquakes with magnitudes 5.5+ 
-df2 <- df[22944:23412,] #subset data for observastions only in the year 2016
+df2 <- df[22944:23412,] #subset data for observations only in the year 2016
 
 getColor <- function(d2) {
   sapply(df2$Magnitude, function(Magnitude) {
     if(Magnitude <= 5.9) { #green marker if magnitude 5-5.9
       "green"
-    } else if(Magnitude <= 6.9) { #orange marker if amgnitude 5.9-6.9
+    } else if(Magnitude <= 6.9) { #orange marker if magnitude 5.9-6.9
       "orange"
     } else if(Magnitude <= 7.9){ #red marker if magnitude 6.9-7.9
       "red"
-    } else #dark red marker if greater than 7.9
+    } else #dark red marker if magnitude greater than 7.9
       "darkred"})
 }
 icons <- awesomeIcons(
@@ -32,5 +32,10 @@ map <- leaflet(data=df2) %>%
              icon = icons) #show magnitude after hovering over marker
 
 
-map2 <- fitBounds(map,-150,70, -60,30) #create map boundary to display 
+map2 <- fitBounds(map,-150,70, -60,30) #create map boundary to display around North America
 map2
+
+#The graphic was created to illustrate the location and magnitudes of significant earthquakes (5.5+).
+#I chose specific colours based on the classes of earthquakes defined by the range their magnitude resides:
+#https://en.wikipedia.org/wiki/Richter_magnitude_scale
+#It was immediately noticeble that in 2016, no detectable earthquake was present on the east coast of Canada.
